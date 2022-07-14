80f23ed11fc97f7b6105c6a8420db4caa14e7190
pablojg9 <pablojg9@pop-os.localdomain>
2022-07-13 23:50:09 -0300

    microservice, docker, docker-compose

---
+++ product-api/src/test/java/io/github/pablojg9/product/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pablojg9.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
